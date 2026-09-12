7232a2422f302686f0176745b06253715ed5f65f
Bel <zephyr9673@gmail.com>
2026-09-13 04:00:52 +0930

    Remove declaration generation from Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
     react({
       jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
     }),
   ],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
+  },
+  build: {
+    sourcemap: false,
+    reportCompressedSize: false,
   },
 });
