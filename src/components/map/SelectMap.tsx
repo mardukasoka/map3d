fe29cb9d0a132d2d577543b15486bb974121ba74
Bel <zephyr9673@gmail.com>
2026-09-13 03:19:19 +0930

    Connect map zoom and bounds to live layer state

--- src/components/map/SelectMap.tsx
+++ src/components/map/SelectMap.tsx
@@ -9,11 +9,44 @@ import L, { LatLng, LatLngBounds } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { css } from "@emotion/react";
 import { CircleMinus, MousePointerClick } from "lucide-react";
+import { useLiveLayerStore } from "../../state/liveLayerStore";
 
 const IconSize = css({
   width: "14px",
   height: "14px",
 });
+
+function LiveViewportObserver() {
+  const setViewport = useLiveLayerStore((state) => state.setViewport);
+  const map = useMapEvents({
+    moveend() {
+      syncViewport();
+    },
+    zoomend() {
+      syncViewport();
+    },
+  });
+
+  const syncViewport = () => {
+    const bounds = map.getBounds();
+    setViewport({
+      zoom: map.getZoom(),
+      bounds: {
+        west: bounds.getWest(),
+        south: bounds.getSouth(),
+        east: bounds.getEast(),
+        north: bounds.getNorth(),
+      },
+    });
+  };
+
+  useEffect(() => {
+    syncViewport();
+    return () => setViewport(null);
+  }, [map, setViewport]);
+
+  return null;
+}
 
 function RectangleSelector({
   isDrag = true,
@@ -88,7 +121,7 @@ function RectangleSelector({
       }
     };
 
-    const handleTouchEnd = (e: TouchEvent) => {
+    const handleTouchEnd = () => {
       if (firstPoint) {
         const latlng = lastLatlngRef.current || firstPoint;
 
@@ -107,7 +140,7 @@ function RectangleSelector({
       container.removeEventListener("touchmove", handleTouchMove);
       container.removeEventListener("touchend", handleTouchEnd);
     };
-  }, [map, isDrag, firstPoint, onChange]);
+  }, [map, isDrag, firstPoint, onChange, onDrawChange]);
 
   useEffect(() => {
     if (map) {
@@ -232,6 +265,7 @@ export function MapComponent({
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <LiveViewportObserver />
         <RectangleSelector
           bounds={bounds}
           drawBounds={drawBounds}
